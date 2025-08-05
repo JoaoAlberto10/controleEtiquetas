66a57f407bce589ac12aeac3b9f28a117f2dab39
JoaoAlberto10 <joaoalberto808@gmail.com>
2025-08-04 23:45:03 -0300

    Atualização e inclusão de Regra de Quantidade Acima do Pedido

--- conferenciaTGFCOI2.java
+++ conferenciaTGFCOI2.java
@@ -10,126 +10,86 @@ import java.sql.ResultSet;
 
 public class conferenciaTGFCOI2 implements EventoProgramavelJava {
 
-    @Override
-    public void beforeInsert(PersistenceEvent event) throws Exception {
-        DynamicVO notaVO = (DynamicVO) event.getVo();
-        BigDecimal nuConf = notaVO.asBigDecimal("NUCONF");
-        String codBarra = notaVO.asString("CODBARRA");
-        String codVolNovo = notaVO.asString("CODVOL");
-        BigDecimal codProd = notaVO.asBigDecimal("CODPROD");
-
-        BigDecimal top = null;
-        BigDecimal codVolPedido = null;
-        BigDecimal codBarraConferido = null;
-
-        // Consulta TOP
-        NativeSql sql = new NativeSql(event.getJdbcWrapper());
-        sql.appendSql(
-                "SELECT DISTINCT CAB.CODTIPOPER AS CODTIPOPER " +
-                        "FROM TGFCON2 CON " +
-                        "JOIN TGFCAB CAB ON CAB.NUNOTA = CON.NUNOTAORIG " +
-                        "WHERE CON.NUCONF = :NUCONF"
-        );
-        sql.setNamedParameter("NUCONF", nuConf);
-        ResultSet rs = sql.executeQuery();
-        if (rs.next()) {
-            top = rs.getBigDecimal("CODTIPOPER");
-        }
-        rs.close();
-
-        if (top != null && (top.intValue() == 406 || top.intValue() == 407)) {
-            NativeSql sql2 = new NativeSql(event.getJdbcWrapper());
-            sql2.appendSql(
-                    "SELECT " +
-                            "(SELECT COUNT(*) FROM TGFCOI2 WHERE NUCONF = :NUCONF AND CODBARRA = :CODBARRA) AS CODBARRAREPETIDO, " +
-                            "(SELECT COUNT(*) FROM TGFCON2 CON " +
-                            "JOIN TGFITE ITE ON ITE.NUNOTA = CON.NUNOTAORIG " +
-                            "WHERE CON.NUCONF = :NUCONF AND ITE.CODPROD = :CODPROD AND ITE.CODVOL = :CODVOL) AS CODVOLPEDIDO " +
-                            "FROM DUAL"
-            );
-            sql2.setNamedParameter("NUCONF", nuConf);
-            sql2.setNamedParameter("CODPROD", codProd);
-            sql2.setNamedParameter("CODVOL", codVolNovo);
-            sql2.setNamedParameter("CODBARRA", codBarra);
-            ResultSet rs2 = sql2.executeQuery();
-
-            if (rs2.next()) {
-                codBarraConferido = rs2.getBigDecimal("CODBARRAREPETIDO");
-                codVolPedido = rs2.getBigDecimal("CODVOLPEDIDO");
-            }
-            rs2.close();
-
-            if (codVolPedido == null || codVolPedido.compareTo(BigDecimal.ZERO) == 0) {
-                throw new MGEModelException("VOLUME INFORMADO NÃO FAZ PARTE DO PEDIDO ORIGINAL!");
-            }
-
-            if (codBarraConferido != null && codBarraConferido.compareTo(BigDecimal.ZERO) > 0) {
-                throw new MGEModelException("PRODUTO JÁ FOI CONTABILIZADO!");
-            }
-        }
+    @Override public void beforeInsert(PersistenceEvent event) throws Exception {
+        validarConferencia((DynamicVO) event.getVo(), event);
     }
-
     @Override public void beforeUpdate(PersistenceEvent event) throws Exception {
-        DynamicVO notaVO = (DynamicVO) event.getVo();
-        BigDecimal nuConf = notaVO.asBigDecimal("NUCONF");
-        String codBarra = notaVO.asString("CODBARRA");
-        String codVolNovo = notaVO.asString("CODVOL");
-        BigDecimal codProd = notaVO.asBigDecimal("CODPROD");
-
-        BigDecimal top = null;
-        BigDecimal codVolPedido = null;
-        BigDecimal codBarraConferido = null;
-
-        // Consulta TOP
-        NativeSql sql = new NativeSql(event.getJdbcWrapper());
-        sql.appendSql(
-                "SELECT DISTINCT CAB.CODTIPOPER AS CODTIPOPER " +
-                        "FROM TGFCON2 CON " +
-                        "JOIN TGFCAB CAB ON CAB.NUNOTA = CON.NUNOTAORIG " +
-                        "WHERE CON.NUCONF = :NUCONF"
-        );
-        sql.setNamedParameter("NUCONF", nuConf);
-        ResultSet rs = sql.executeQuery();
-        if (rs.next()) {
-            top = rs.getBigDecimal("CODTIPOPER");
-        }
-        rs.close();
-
-        if (top != null && (top.intValue() == 406 || top.intValue() == 407)) {
-            NativeSql sql2 = new NativeSql(event.getJdbcWrapper());
-            sql2.appendSql(
-                    "SELECT " +
-                            "(SELECT COUNT(*) FROM TGFCOI2 WHERE NUCONF = :NUCONF AND CODBARRA = :CODBARRA) AS CODBARRAREPETIDO, " +
-                            "(SELECT COUNT(*) FROM TGFCON2 CON " +
-                            "JOIN TGFITE ITE ON ITE.NUNOTA = CON.NUNOTAORIG " +
-                            "WHERE CON.NUCONF = :NUCONF AND ITE.CODPROD = :CODPROD AND ITE.CODVOL = :CODVOL) AS CODVOLPEDIDO " +
-                            "FROM DUAL"
-            );
-            sql2.setNamedParameter("NUCONF", nuConf);
-            sql2.setNamedParameter("CODPROD", codProd);
-            sql2.setNamedParameter("CODVOL", codVolNovo);
-            sql2.setNamedParameter("CODBARRA", codBarra);
-            ResultSet rs2 = sql2.executeQuery();
-
-            if (rs2.next()) {
-                codBarraConferido = rs2.getBigDecimal("CODBARRAREPETIDO");
-                codVolPedido = rs2.getBigDecimal("CODVOLPEDIDO");
-            }
-            rs2.close();
-
-            if (codVolPedido == null || codVolPedido.compareTo(BigDecimal.ZERO) == 0) {
-                throw new MGEModelException("VOLUME INFORMADO NÃO FAZ PARTE DO PEDIDO ORIGINAL!");
-            }
-
-            if (codBarraConferido != null && codBarraConferido.compareTo(BigDecimal.ZERO) > 0) {
-                throw new MGEModelException("PRODUTO JÁ FOI CONTABILIZADO!");
-            }
-        }
+        validarConferencia((DynamicVO) event.getVo(), event);
     }
-
     @Override public void afterUpdate(PersistenceEvent event) throws Exception {}
     @Override public void beforeDelete(PersistenceEvent event) {}
     @Override public void afterInsert(PersistenceEvent event) {}
     @Override public void afterDelete(PersistenceEvent event) {}
     @Override public void beforeCommit(TransactionContext tranCtx) {}
+
+    private void validarConferencia(DynamicVO notaVO, PersistenceEvent event) throws Exception {
+        BigDecimal nuConf = notaVO.asBigDecimal("NUCONF");
+        String codBarra = notaVO.asString("CODBARRA");
+        String codVolNovo = notaVO.asString("CODVOL");
+        BigDecimal codProd = notaVO.asBigDecimal("CODPROD");
+        BigDecimal qtdconf = notaVO.asBigDecimal("QTDCONF");
+
+        BigDecimal top = null;
+        BigDecimal codVolPedido = null;
+        BigDecimal codBarraConferido = null;
+        BigDecimal qtdPendente = null;
+
+        // Consulta TOP
+        NativeSql sql = new NativeSql(event.getJdbcWrapper());
+        sql.appendSql(
+                "SELECT DISTINCT CAB.CODTIPOPER AS CODTIPOPER " +
+                        "FROM TGFCON2 CON " +
+                        "JOIN TGFCAB CAB ON CAB.NUNOTA = CON.NUNOTAORIG " +
+                        "WHERE CON.NUCONF = :NUCONF"
+        );
+        sql.setNamedParameter("NUCONF", nuConf);
+        ResultSet rs = sql.executeQuery();
+        if (rs.next()) {
+            top = rs.getBigDecimal("CODTIPOPER");
+        }
+        rs.close();
+
+        if (top != null && (top.intValue() == 406 || top.intValue() == 407)) {
+            NativeSql sql2 = new NativeSql(event.getJdbcWrapper());
+            sql2.appendSql(
+                    "SELECT " +
+                            "(SELECT COUNT(*) FROM TGFCOI2 WHERE NUCONF = :NUCONF AND CODBARRA = :CODBARRA) AS CODBARRAREPETIDO, " +
+                            "(SELECT COUNT(*) FROM TGFCON2 CON " +
+                            "JOIN TGFITE ITE ON ITE.NUNOTA = CON.NUNOTAORIG " +
+                            "WHERE CON.NUCONF = :NUCONF AND ITE.CODPROD = :CODPROD AND ITE.CODVOL = :CODVOL) AS CODVOLPEDIDO, " +
+                            "NVL((SELECT SUM(CASE WHEN ITE.CODVOL=PRO.CODVOL THEN ITE.QTDNEG ELSE " +
+                            "ITE.QTDNEG/VOA.QUANTIDADE END) AS QTDITENS FROM TGFCON2 CON " +
+                            "JOIN TGFITE ITE ON ITE.NUNOTA = CON.NUNOTAORIG " +
+                            "INNER JOIN TGFPRO PRO ON PRO.CODPROD=ITE.CODPROD " +
+                            "LEFT JOIN TGFVOA VOA ON VOA.CODPROD=ITE.CODPROD AND VOA.CODVOL=ITE.CODVOL " +
+                            "WHERE CON.NUCONF = :NUCONF AND ITE.CODPROD = :CODPROD AND ITE.CODVOL = :CODVOL),0) - " +
+                            "NVL((SELECT SUM(QTDCONF) FROM TGFCOI2 WHERE NUCONF = :NUCONF AND CODPROD= :CODPROD AND CODVOL = :CODVOL),0) AS QTDPENDENTE " +
+                            "FROM DUAL"
+            );
+            sql2.setNamedParameter("NUCONF", nuConf);
+            sql2.setNamedParameter("CODPROD", codProd);
+            sql2.setNamedParameter("CODVOL", codVolNovo);
+            sql2.setNamedParameter("CODBARRA", codBarra);
+            ResultSet rs2 = sql2.executeQuery();
+
+            if (rs2.next()) {
+                codBarraConferido = rs2.getBigDecimal("CODBARRAREPETIDO");
+                codVolPedido = rs2.getBigDecimal("CODVOLPEDIDO");
+                qtdPendente = rs2.getBigDecimal("QTDPENDENTE");
+            }
+            rs2.close();
+
+            if (qtdconf.compareTo(qtdPendente) > 0) {
+                throw new MGEModelException("Quantidade acima do pedido original!");
+            }
+
+            if (codVolPedido == null || codVolPedido.compareTo(BigDecimal.ZERO) == 0) {
+                throw new MGEModelException("Volume informado não faz parte do pedido original!");
+            }
+
+            if (codBarraConferido != null && codBarraConferido.compareTo(BigDecimal.ZERO) > 0) {
+                throw new MGEModelException("Este código de barras já foi conferido anteriormente!");
+            }
+        }
+    }
 }
